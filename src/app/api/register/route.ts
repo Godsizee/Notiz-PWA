c01e2c4d2de58bbe7b967506b70d3edfbae88061
Godsizee <badesebastian@outlook.com>
2026-06-16 14:22:07 +0200

    1

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -8,7 +8,7 @@ const ALLOWED_EMAILS = [
   "annaklatsche83@gmail.com",
 ];
 
-const PB_URL = process.env.NEXT_PUBLIC_POCKETBASE_URL || "https://pb.dasdann.jetzt";
+const PB_URL = process.env.NEXT_PUBLIC_POCKETBASE_URL || "https://pbnote.dasdann.jetzt";
 
 export async function POST(request: Request) {
   try {
